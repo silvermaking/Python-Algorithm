f2732c3bb01b0df753f90bec3ca51c73a1941d2c
이은성 <silvermaking@gmail.com>
2021-09-13 17:48:35 +0900

    210913 SWEA

---
+++ SWEA/5603.건초더미.py
@@ -0,0 +1,28 @@
+#백준(1018) - 실버V.체스판 다시 칠하기
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    guncho = [int(input()) for _ in range(N)]
+    m = sum(guncho)//len(guncho)
+    cnt = 0
+    while True:
+        if min(guncho) == m:
+            break
+        for i in range(len(guncho)):
+            if guncho[i] < m:
+                cnt += m - guncho[i]
+                guncho[i] = m
+                break
+    print(f'#{tc} {cnt}')
+'''
+20 
+5
+2 10 7 1
+2  9 7 2
+2  8 7  3
+3  7 7  3
+4  6 7  3
+4  6 6  4
+5  5 5  4
+5  5 5 5 
+'''
