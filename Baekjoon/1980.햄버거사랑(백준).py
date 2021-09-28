17cd1961472d9a59a1c193a182a300ef5b2600c9
이은성 <silvermaking@gmail.com>
2021-09-28 17:49:12 +0900

    210928 baekjoon

---
+++ Baekjoon/1980.햄버거사랑(백준).py
@@ -0,0 +1,35 @@
+'''
+구현, brute-force
+
+'''
+import sys
+n, m, t = map(int, sys.stdin.readline().strip().split())
+coke = 0
+temp_t = t
+cnt = 0
+while True:
+    #종료조건
+    if temp_t == 0:
+        break
+
+    # 나누어 안떨어지면 coke 한개 씩 추가
+    if temp_t < 0:
+        coke += 1
+        temp_t = t - coke
+        cnt = 0
+    #적은 타임의 햄버거수가 더 많게
+    if n <= m:
+        if temp_t % n == 0:
+            cnt += temp_t // n
+            temp_t = 0
+        else:
+            temp_t -= m
+            cnt += 1
+    else:
+        if temp_t % m == 0:
+            cnt += temp_t // m
+            temp_t = 0
+        else:
+            temp_t -= n
+            cnt += 1
+print(f'{cnt} {coke}')
