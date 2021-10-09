149a14a0134bffa71af44b684e2ff3059ad8c89b
이은성 <silvermaking@gmail.com>
2021-10-09 14:04:51 +0900

    SWEA

---
+++ SWEA/5431.민석이의과제체크하기.py
@@ -0,0 +1,10 @@
+for tc in range(1, int(input()) + 1):
+    # N : 학생 수 K : 과제 제출한 수
+    N, K = map(int, input().split())
+    # 과제 제출하지 않은 사람
+    K_lst = list(map(int, input().split()))
+    answer = [i for i in range(1, N+1) if i not in K_lst]
+    print(f'#{tc}', end=" ")
+    for ans in answer:
+        print(ans, end=" ")
+    print()
